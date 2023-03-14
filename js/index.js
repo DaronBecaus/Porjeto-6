66bef24e4a8942ba79093c2310d29d56ef76e713
DaronBecaus <danielsilvasoder@gmail.com>
2023-03-13 21:44:55 -0300

    2

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function trocaCor() {
+  document.getElementById("hover").style.backgroundColor = "red";
+}
+
+function voltaCor() {
+  document.getElementById("hover").style.backgroundColor = "blueviolet";
+}
